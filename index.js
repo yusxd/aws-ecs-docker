531970e2b47722be88bd673e41b5b55d2021f5bd
gauri17-pro <60473255+gauri17-pro@users.noreply.github.com>
2023-01-31 22:12:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("welcome to express app!!");
+  res.send("Hey guys, Welcome to my new Express Application!!");
 });
 
 app.get("/info", (req, res) => {
